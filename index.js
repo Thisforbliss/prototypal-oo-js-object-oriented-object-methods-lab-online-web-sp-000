9bf8dfaad451fa55c906ae5895bcc958c75265d6
Malcolm Leake <echelon1089@gmail.com>
2020-04-15 23:39:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class BoardMember {
       return "You will see great things from Scuber."
     }
     this.sayHi = function(){
-      `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
+      return `Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.`
     }
     
   }
